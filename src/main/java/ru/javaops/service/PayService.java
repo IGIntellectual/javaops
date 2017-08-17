e200471bc0af40cff7281a29b8bf41c2c621e9db
Grigory Kislin <gkislin@yandex.ru>
2017-08-17 23:19:51 +0300

    Fix pay service

--- src/main/java/ru/javaops/service/PayService.java
+++ src/main/java/ru/javaops/service/PayService.java
@@ -33,19 +33,22 @@ public class PayService {
     public String sendPaymentRefMail(UserGroup ug) {
         User user = ug.getUser();
         User refUser = refService.getRefUser(ug.getUser());
-        if (ug.getRegisterType() == RegisterType.DUPLICATED) {
-            log.info("Reference user {} from {} already present in group {}", user, refUser.getEmail(), ug.getGroup().getName());
-
-        } else if (ParticipationType.isParticipant(ug.getParticipationType())) {
-            if (refUser != null) {
-                String project = ug.getGroup().getProject().getName();
+        Group group = ug.getGroup();
+        if (refUser != null && group.isMembers()) {
+            String msg;
+            if (ug.getRegisterType() == RegisterType.DUPLICATED) {
+                msg = String.format("User %s from reference %s already present in group %s", user, refUser.getEmail(), group.getName());
+            } else {
+                String project = group.getProject().getName();
                 int addBonus = "topjava".equals(project) || "masterjava".equals(project) ? 25 : 10;
                 refUser.addBonus(addBonus);
-                log.info("!!! Ref Participation from user {}, bonus {}", refUser.getEmail(), refUser.getBonus());
                 refUser = userService.save(refUser);
+                msg = String.format("User %s from reference (%s, bonus=%d) registered in group %s", user, refUser.getEmail(), refUser.getBonus(), group.getName());
                 refService.sendAsyncMail(refUser, "ref/refParticipation", ImmutableMap.of("project", project, "email", user.getEmail(), "addBonus", addBonus));
             }
+            log.info(msg);
+            return msg;
         }
-        return refUser == null ? "" : "Reference from " + refUser.getEmail() + ", bonus=" + refUser.getBonus() + '\n';
+        return "";
     }
 }
